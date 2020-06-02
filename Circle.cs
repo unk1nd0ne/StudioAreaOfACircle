2778d2312d2702e2d6314a907a9e59327afdc189
unk1nd0ne <z.neil.schaefer@gmail.com>
2020-06-02 08:43:42 -0500

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioAreaOfACircle
+{
+    class Circle
+    {
+        public static double CalculateArea(double radius)
+        {
+            double area;
+
+            area = Math.Pow(radius, 2) * Math.PI;
+
+            return area;
+        }
+        public static double CalculateCircumference(double radius)
+        {
+            double circumference;
+
+            circumference = 2 * Math.PI * radius;
+
+            return circumference;
+        }
+
+        public static double CalculateDiameter(double radius)
+        {
+            double diameter;
+
+            diameter = 2 * radius;
+
+            return diameter;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace StudioAreaOfACircle
+{
+    class Program
+    {
+        private static void Main()
+        {
+            double radius;
+            double area;
+            double circumference;
+            double diameter;
+            string input;
+            double repetitions;
+
+
+            Console.Write("Enter a radius: ");
+            input = Console.ReadLine();
+            radius = double.Parse(input);
+            area = Circle.CalculateArea(radius);
+            Console.WriteLine("The area of a circle of radius " + radius + " is: " + Math.Round(area, 3));
+            circumference = Circle.CalculateCircumference(radius);
+            diameter = Circle.CalculateDiameter(radius);
+            Console.WriteLine("The circumference is: " + Math.Round(circumference, 3) + " and the diameter is: " + Math.Round(diameter, 3));
+            Console.Write("Enter you MPG: ");
+            input = Console.ReadLine();
+            repetitions = double.Parse(input) / circumference;
+            Console.WriteLine("At " + input + " MPG, you can drive " + Math.Round(repetitions, 3) + " times around the circle.");
+
+        }
+    }
+}
